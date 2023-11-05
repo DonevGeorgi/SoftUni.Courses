01f742f3a97ed5d633a264e07eb3cdf80f648558
Georgi Donev <72747971+DonevGeorgi@users.noreply.github.com>
2023-11-05 16:46:05 +0000

    ChangesIsFoldersInBasicCourse

--- SoftUni-ProgramingBasicCSharp/01.FirstStepsInCoding/E01.USDToBGN/Program.cs
+++ SoftUni-ProgramingBasicCSharp/01.FirstStepsInCoding/E01.USDToBGN/Program.cs
@@ -4,7 +4,7 @@ namespace E01.USDToBGN
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             double uSD = double.Parse(Console.ReadLine());
 
