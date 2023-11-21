d8ad2cc16ac39fd59bee57a2cd04ad1909a32625
Georgi Donev <72747971+DonevGeorgi@users.noreply.github.com>
2023-11-21 20:56:39 +0200

    Add files via upload

---
+++ C#Fundamentals/Final-Exam-Fundametals 23.10.2022/P01. CookingMasterclass/CookingMasterclass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P01._CookingMasterclass
+{
+    internal class CookingMasterclass
+    {
+        static void Main()
+        {
+            //Input
+            decimal budget = decimal.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            decimal priceOfFlour = decimal.Parse(Console.ReadLine());
+            decimal priceaOfEgg = decimal.Parse(Console.ReadLine());
+            decimal priceForApron = decimal.Parse(Console.ReadLine());
+
+            decimal freePackageOfFlour = (students / 5) * priceOfFlour;
+
+            decimal totalPriceOfFlour = (priceOfFlour * students) - freePackageOfFlour;
+            decimal totalPriceOfEggs = (priceaOfEgg * 10) * students;
+            decimal bonusAprin = Math.Ceiling(students * 0.20m);
+            decimal totalPriceOfAprons = priceForApron * (students + bonusAprin);
+
+            decimal finalPrice = totalPriceOfFlour + totalPriceOfEggs + totalPriceOfAprons;
+
+            //Output
+            if (finalPrice <= budget)
+            {
+                Console.WriteLine($"Items purchased for {finalPrice:f2}$.");
+            }
+            else if (finalPrice > budget)
+            {
+                Console.WriteLine($"{finalPrice - budget:f2}$ more needed.");
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/Final-Exam-Fundametals 23.10.2022/P02.FriendListMaintenance/FriendListMaintenance.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+
+namespace P02.FriendListMaintenance
+{
+    internal class FriendListMaintenance
+    {
+        static void Main()
+        {
+            //Input
+            string[] friendsList = Console.ReadLine()
+                .Split(", ")
+                .ToArray();
+
+            int countOfBlacklistedNames = 0;
+            int countOfLostlistedNames = 0;
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "Report")
+            {
+                string[] inputArgs = input.Split();
+
+                if (inputArgs[0] == "Blacklist")
+                {
+                    string name = inputArgs[1];
+
+                    bool notFound = true;
+
+                    for (int i = 0; i < friendsList.Length; i++)
+                    {
+                        if (friendsList[i] == name)
+                        {
+                            notFound = false;
+                            friendsList[i] = "Blacklisted";
+                            Console.WriteLine($"{name} was blacklisted.");
+                            break;
+                        }
+                    }
+
+                    if (notFound)
+                    {
+                        Console.WriteLine($"{name} was not found.");
+                    }
+
+                }
+                else if (inputArgs[0] == "Error")
+                {
+                    int index = int.Parse(inputArgs[1]);
+
+                    if (index < 0 || index >= friendsList.Length || friendsList[index] == "Blacklisted" || friendsList[index] == "Lost")
+                    {
+                        continue;
+                    }
+
+                    Console.WriteLine($"{friendsList[index]} was lost due to an error.");
+                    friendsList[index] = "Lost";
+
+                }
+                else if (inputArgs[0] == "Change")
+                {
+                    int index = int.Parse((inputArgs[1]));
+                    string newName = inputArgs[2];
+
+                    if (index < 0 || index >= friendsList.Length)
+                    {
+                        continue;
+                    }
+
+                    Console.WriteLine($"{friendsList[index]} changed his username to {newName}.");
+                    friendsList[index] = newName;
+
+                }
+            }
+
+            for (int i = 0; i < friendsList.Length; i++)
+            {
+                if (friendsList[i] == "Blacklisted")
+                {
+                    countOfBlacklistedNames++;
+                }
+                else if (friendsList[i] == "Lost")
+                {
+                    countOfLostlistedNames++;
+                }
+            }
+
+            Console.WriteLine($"Blacklisted names: {countOfBlacklistedNames}");
+            Console.WriteLine($"Lost names: {countOfLostlistedNames}");
+            Console.WriteLine(string.Join(" ", friendsList));
+        }
+    }
+}
