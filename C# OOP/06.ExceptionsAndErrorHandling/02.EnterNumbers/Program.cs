9fd8552315061f3ab9fc2bbf9f6cca626797be3f
Georgi Donev <72747971+DonevGeorgi@users.noreply.github.com>
2023-12-18 10:23:33 +0200

    Add files via upload

---
+++ C# OOP/06.ExceptionsAndErrorHandling/02.EnterNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnterNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new();
+
+            while (numbers.Count < 10)
+            {
+                try
+                {
+                    int number = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        static void ReadNumber(int start, int end)
+        {
+
+        }
+    }
+}
---
+++ C# OOP/06.ExceptionsAndErrorHandling/E03.Cards/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cards
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Card> deck = new();
+
+            string[] cardsInfo = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < cardsInfo.Length; i++)
+            {
+                string[] cardInfo = cardsInfo[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string face = cardInfo[0];
+                string suit = cardInfo[1];
+
+                try
+                {
+                    Card card = Card.CreateCard(face, suit);
+                    deck.Add(card);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', deck));
+        }
+    }
+
+    class Card
+    {
+        private string Face { get; set; }
+        private string Suit { get; set; }
+
+        public static Card CreateCard(string face, string suit)
+        {
+            List<string> validFaces = new() { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+            List<string> validSuits = new() { "S", "H", "D", "C" };
+
+            if (!validFaces.Any(f => f == face) || !validSuits.Any(s => s == suit))
+            {
+                throw new ArgumentException("Invalid card!");
+            }
+
+            switch (suit)
+            {
+                case "S":
+                    suit = "\u2660";
+                    break;
+                case "H":
+                    suit = "\u2665";
+                    break;
+                case "D":
+                    suit = "\u2666";
+                    break;
+                case "C":
+                    suit = "\u2663";
+                    break;
+            }
+
+            return new() { Face = face, Suit = suit };
+        }
+
+        public override string ToString()
+        => $"[{Face}{Suit}]";
+    }
+}
