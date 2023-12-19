b4eae02ec483ed367a95c4cee0e7a1632f8f09cd
Georgi Donev <72747971+DonevGeorgi@users.noreply.github.com>
2023-12-19 11:18:20 +0200

    Add files via upload

---
+++ C# OOP/ExamPreparations/Models/Cars/SuperCar.cs
@@ -0,0 +1,12 @@
+﻿namespace CarRacing.Models.Cars
+{
+    public class SuperCar : Car
+    {
+        private const double InitialFuelAvailable = 80;
+        private const double InitialFuelConsumption = 10;
+        public SuperCar(string make, string model, string VIN, int horsePower)
+            : base(make, model, VIN, horsePower, InitialFuelAvailable, InitialFuelConsumption)
+        {
+        }
+    }
+}
