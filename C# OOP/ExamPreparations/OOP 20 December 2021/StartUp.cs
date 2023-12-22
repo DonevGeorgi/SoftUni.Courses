e0cecde3113990feb447be90ba9e1d915b2637f5
Georgi Donev <72747971+DonevGeorgi@users.noreply.github.com>
2023-12-22 09:54:03 +0200

    Add files via upload

---
+++ C# OOP/ExamPreparations/OOP 20 December 2021/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace NavalVessels
+{
+    using System;
+    using Core;
+    using Core.Contracts;
+    using NavalVessels.Models;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            IEngine engine = new Engine();
+            engine.Run();
+
+
+        }
+    }
+}
